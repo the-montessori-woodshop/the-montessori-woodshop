33d936d2ee52f99a717ed74879d842c3ec8131e6
Drew DeCarme <drewdecarme@gmail.com>
2021-11-10 22:53:08 -0500

    feat: Add storefront, medusa and admin

---
+++ medusa/medusa-config.js
@@ -0,0 +1,25 @@
+// This is the place to include plugins. See API documentation for a thorough guide on plugins.
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  // Uncomment to add Stripe support.
+  // You can create a Stripe account via: https://stripe.com
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: process.env.STRIPE_API_KEY,
+  //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+  //   },
+  // },
+];
+
+module.exports = {
+  projectConfig: {
+    redis_url: process.env.MEDUSA_REDIS_URL,
+    database_url: process.env.MEDUSA_DATABASE_URL,
+    database_type: "postgres",
+    store_cors: process.env.MEDUSA_STORE_CORS,
+    admin_cors: process.env.MEDUSA_ADMIN_CORS,
+  },
+  plugins,
+};
