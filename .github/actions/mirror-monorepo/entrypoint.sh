5f68803924c62185729f8225f9216a600ac82b60
Drew DeCarme <drewdecarme@gmail.com>
2021-12-20 12:59:19 -0500

    ci: Add repo name to action

--- .github/actions/mirror-monorepo/entrypoint.sh
+++ .github/actions/mirror-monorepo/entrypoint.sh
@@ -2,11 +2,12 @@
 
 USER_EMAIL="$1"
 USER_NAME="$2"
+REPO_NAME="$2"
 
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
-git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/woodshop-store.git"
+git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/$REPO_NAME.git"
 git fetch https://github.com/the-montessori-woodshop/the-montessori-woodshop.git main && git cherry-pick FETCH_HEAD
 git push -u main
