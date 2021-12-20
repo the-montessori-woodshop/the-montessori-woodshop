1f7e6eb49ab45cb58ef5fc533ab6c2e3f55eaa00
Drew DeCarme <drewdecarme@gmail.com>
2021-12-20 12:46:09 -0500

    update entrypoint

--- .github/actions/copy-to-admin/entrypoint.sh
+++ .github/actions/copy-to-admin/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+USER_EMAIL="$1"
+USER_NAME="$2"
+
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 git config --global user.email "$USER_EMAIL"
