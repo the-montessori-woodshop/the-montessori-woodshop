aae0ec7a1534835d25e870fcd5793d4c1bc428bb
Drew DeCarme <drewdecarme@gmail.com>
2021-12-20 12:42:14 -0500

    update entrypoint

--- .github/actions/copy-to-admin/entrypoint.sh
+++ .github/actions/copy-to-admin/entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+# git config --global user.email "$USER_EMAIL"
+# git config --global user.name "$USER_NAME"
+git config --global user.email drewdecarme@gmail.com
+git config --global user.name Drew DeCarme
 # git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
 git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/woodshop-store.git"
 git fetch https://github.com/the-montessori-woodshop/the-montessori-woodshop.git main && git cherry-pick FETCH_HEAD
