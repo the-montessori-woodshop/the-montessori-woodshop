c15f0059c78fee8c1956d1a166ab29da4393818e
Drew DeCarme <drewdecarme@gmail.com>
2022-01-04 16:21:05 -0500

    add action back in

---
+++ .github/actions/mirror-monorepo/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -l
+
+USER_EMAIL="$1"
+USER_NAME="$2"
+REPO_NAME="$3"
+
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/$REPO_NAME.git"
+cd "$REPO_NAME"
+git fetch https://github.com/the-montessori-woodshop/the-montessori-woodshop.git main && git merge FETCH_HEAD --allow-unrelated-histories
+git push -u "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/$REPO_NAME.git" main
