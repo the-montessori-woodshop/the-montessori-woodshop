53aa593678fd836c140926b70ed03b6b48b44a1d
Drew DeCarme <drewdecarme@gmail.com>
2022-01-06 16:02:12 -0500

    update action

---
+++ .github/actions/copy-to-store/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+USER_EMAIL="$1"
+USER_NAME="$2"
+REPO_NAME="$3"
+
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+git clone --single-branch --branch main "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/woodshop-store.git"
+
+cd "woodshop-store"
+git fetch https://github.com/the-montessori-woodshop/the-montessori-woodshop.git main && git merge FETCH_HEAD --allow-unrelated-histories
+
+rm -r ./packages/components
+cp -r ../packages/components ./packages/components
+
+git commit -m "chore: Replace components"
+git push -u "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/the-montessori-woodshop/woodshop-store.git" main
+
+# ls -all
