afe41daf5b6d7ac6cd56ff8f36daf5b4bb3d559c
Drew DeCarme <drewdecarme@gmail.com>
2022-01-18 11:22:18 -0500

    add docs / remove file plugin

--- packages/docs/stories/Button.stories.tsx
+++ packages/docs/stories/Button.stories.tsx
@@ -3,12 +3,8 @@ import { Button } from "@woodshop/components";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  title: "Atoms / Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -17,8 +13,9 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  cxColor: "primary",
+  cxColor: "secondary",
   cxVariant: "text",
+  children: "Text!",
 };
 
 // export const Secondary = Template.bind({});
