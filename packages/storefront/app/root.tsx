e8b44c9253867127dd94e5a1e1d3adf3fc3dd468
Drew DeCarme <drewdecarme@gmail.com>
2022-02-16 13:45:04 -0500

    update favicons and such

--- packages/storefront/app/root.tsx
+++ packages/storefront/app/root.tsx
@@ -62,7 +62,7 @@ export default function App() {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="manifest" href="/site.webmanifest" />
         <Meta />
         <Links />
       </head>
