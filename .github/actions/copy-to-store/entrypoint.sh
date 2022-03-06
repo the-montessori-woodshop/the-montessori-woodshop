d22d9b8e3d5e2418b1b7957359622477c4762940
Drew DeCarme <drewdecarme@gmail.com>
2022-03-06 10:23:42 -0500

    install yarn

--- .github/actions/copy-to-store/entrypoint.sh
+++ .github/actions/copy-to-store/entrypoint.sh
@@ -31,6 +31,7 @@ cp -r packages/axios-fetch tmp/packages/axios-fetch
 
 # Install the packages to create the lockfile to be used
 cd tmp
+npm install --global yarn
 yarn install
 yarn set version berry
 # npm install
