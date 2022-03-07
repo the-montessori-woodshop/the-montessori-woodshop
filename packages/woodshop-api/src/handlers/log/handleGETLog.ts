2bdddd781cc4de4493c6207f3e191e914afa606c
Drew DeCarme <drewdecarme@gmail.com>
2022-03-07 09:48:26 -0500

    infra updates

---
+++ packages/woodshop-api/src/handlers/log/handleGETLog.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+import { HandleGETRequest } from "../../types";
+
+const prisma = new PrismaClient();
+
+const handleGETLog: HandleGETRequest = async (request) => {
+  await prisma.log.create({
+    data: {
+      level: "Info",
+      message: `${request.method} ${request.url}`,
+      meta: {
+        headers: JSON.stringify(request.headers)
+      }
+    }
+  });
+  return new Response(`request method: ${request.method}!`);
+};
+
+export default handleGETLog;
