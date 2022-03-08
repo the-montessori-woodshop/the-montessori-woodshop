3b59b3fa75440cd1f68dabada4f019d05341ee81
Drew DeCarme <drewdecarme@gmail.com>
2022-03-07 23:00:57 -0500

    test

--- packages/woodshop-api/src/handlers/log/handleGETLog.ts
+++ packages/woodshop-api/src/handlers/log/handleGETLog.ts
@@ -10,7 +10,7 @@ const handleGETLog: HandleGETRequest = async (request) => {
   await prisma.log.create({
     data: {
       level: "Info",
-      id: "c5711185-47f2-52d2-b72d-7956a7c9ef51",
+      id: "test-47f2-52d2-b72d-7956a7c9ef51",
       message: `${request.method} ${request.url}`,
       meta: {
         headers: JSON.stringify(request.headers)
