74d28d626a59f11e6fc28826fe56de7b1298264e
Drew DeCarme <drewdecarme@gmail.com>
2022-06-29 10:35:22 -0400

    fix: Remove api base route

--- packages/woodshop-api/src/index.ts
+++ packages/woodshop-api/src/index.ts
@@ -5,7 +5,7 @@ import { PostRouter } from "./features/post/post.route";
 import { UserRouter } from "./features/user/user.route";
 import { handleError } from "./utils/handleError";
 
-const router = Router({ base: "/api" });
+const router = Router({ base: "/" });
 
 // Router
 router
