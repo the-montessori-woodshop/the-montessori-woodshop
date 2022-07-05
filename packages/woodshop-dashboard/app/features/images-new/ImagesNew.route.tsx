8194194a695efb5dae7e1262775e8e01560dc154
Drew DeCarme <drewdecarme@gmail.com>
2022-07-05 17:52:25 -0400

    udpates

--- packages/woodshop-dashboard/app/features/images-new/ImagesNew.route.tsx
+++ packages/woodshop-dashboard/app/features/images-new/ImagesNew.route.tsx
@@ -1,19 +1,33 @@
 import { redirect } from "@remix-run/cloudflare";
+import { Form } from "@remix-run/react";
 import type { POST_NewImageApiResponse } from "@woodshop/api";
+import {
+  Button,
+  ButtonGroup,
+  FormFieldGroup,
+  FormFieldImageDropzone,
+} from "@woodshop/components";
 import { Breadcrumb } from "~/components/breadcrumb";
 import { api } from "~/features//api";
 import type { RemixFeatureUIRoute } from "~/types/routes.types";
-
-import { PageBody, PageHeader, PageHeaderTitle } from "../page";
+import clsx from "clsx";
 
 export const ImagesNewRoute: RemixFeatureUIRoute = () => {
   return (
-    <>
-      <PageHeader>
-        <PageHeaderTitle>Upload a New Image</PageHeaderTitle>
-      </PageHeader>
-      <PageBody>stuff</PageBody>
-    </>
+    <Form className={clsx("lmO0A93k", "container")}>
+      <div className={clsx("lmO0A93k", "main")}>
+        <FormFieldGroup cxLayout="stacked">
+          <FormFieldImageDropzone />
+        </FormFieldGroup>
+      </div>
+      <div className={clsx("lmO0A93k", "footer")}>
+        <ButtonGroup>
+          <Button cxColor="primary" cxVariant="contained" type="submit">
+            Create Image
+          </Button>
+        </ButtonGroup>
+      </div>
+    </Form>
   );
 };
 
